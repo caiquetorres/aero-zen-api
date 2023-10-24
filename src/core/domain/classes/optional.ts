098a230815646177d54649f4e5a215052981d89a
Caique Torres <caiquet.bastos@gmail.com>
2023-10-24 15:26:09 -0300

    feat(optional.ts & result.ts): created some classes and functions to allow using monads

---
+++ src/core/domain/classes/optional.ts
@@ -0,0 +1,51 @@
+declare global {
+  type Optional<T> = Some<T> | None;
+
+  function some<T>(value: T): Some<T>;
+
+  function none(): None;
+}
+
+global.some = function <T>(value: T): Some<T> {
+  return new Some(value);
+};
+
+global.none = function (): None {
+  return new None();
+};
+
+export class Some<T> {
+  constructor(readonly value: T) {
+    Object.freeze(this);
+  }
+
+  isSome(): this is Some<T> {
+    return true;
+  }
+
+  isNone(): this is None {
+    return false;
+  }
+
+  unwrap(): T {
+    return this.value;
+  }
+}
+
+export class None {
+  constructor() {
+    Object.freeze(this);
+  }
+
+  isSome(): this is Some<never> {
+    return false;
+  }
+
+  isNone(): this is None {
+    return true;
+  }
+
+  unwrap(): never {
+    throw new Error('Cannot unwrap a None value.');
+  }
+}
